babb6307d3f02ce7c1f1a51c70f1d8e8096e5e6d
Shakira Weir <shakira.weir87@gmail.com>
2019-01-08 16:32:58 -0500

    create function that displays canvas hanger when page loads

--- dist/main.8f75cf09.js
+++ dist/main.8f75cf09.js
@@ -109,44 +109,53 @@ module.exports = {
   message: function message(messageText) {
     console.log(messageText);
   } // draw hangman stickfigure using canvas API
-  // draw straight lines on canvas
+  // create function that displays canvas hanger when page loads
 
 };
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.moveTo(10, 320);
-ctx.lineTo(120, 320);
-ctx.stroke();
-ctx.moveTo(65, 60);
-ctx.lineTo(65, 320);
-ctx.stroke();
-ctx.moveTo(65, 60);
-ctx.lineTo(220, 60);
-ctx.stroke();
-ctx.moveTo(220, 60);
-ctx.lineTo(220, 110);
-ctx.stroke(); // draw a circle on canvas
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-ctx.arc(220, 140, 30, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.moveTo(220, 170);
-ctx.lineTo(220, 250);
-ctx.stroke();
-ctx.moveTo(220, 250);
-ctx.lineTo(180, 300);
-ctx.stroke();
-ctx.moveTo(220, 250);
-ctx.lineTo(260, 300);
-ctx.stroke();
-ctx.moveTo(220, 210);
-ctx.lineTo(180, 190);
-ctx.stroke();
-ctx.moveTo(220, 210);
-ctx.lineTo(260, 190);
-ctx.stroke();
+window.onload = function displayHanger() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.moveTo(10, 320);
+  ctx.lineTo(120, 320);
+  ctx.stroke();
+  ctx.moveTo(65, 60);
+  ctx.lineTo(65, 320);
+  ctx.stroke();
+  ctx.moveTo(65, 60);
+  ctx.lineTo(220, 60);
+  ctx.stroke();
+  ctx.moveTo(220, 60);
+  ctx.lineTo(220, 110);
+  ctx.stroke();
+}; // hanging hangman
+//display hangman onload
+//hide hangman figure
+//reveal a part of stick figure for every incorrect guess
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+// ctx.beginPath();
+// ctx.arc(220, 140, 30, 0, 2 * Math.PI);
+// ctx.stroke();
+// ctx.moveTo(220,170);
+// ctx.lineTo(220,250);
+// ctx.stroke();
+// ctx.moveTo(220,250);
+// ctx.lineTo(180,300);
+// ctx.stroke();
+// ctx.moveTo(220,250);
+// ctx.lineTo(260,300);
+// ctx.stroke();
+// ctx.moveTo(220,210);
+// ctx.lineTo(180,195);
+// ctx.stroke();
+// ctx.moveTo(220,210);
+// ctx.lineTo(260,195);
+// ctx.stroke();
+// create JS function to respond to player 1 button by adding text onClick (acceptWord)
+//create function to display blank spaces (displayBlankSpaces)
+//create function to hide word entry display?
+//create function to process letter (processLetter)
 },{}],"../js/main.js":[function(require,module,exports) {
 var messages = require("./message");
 
@@ -178,7 +187,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64494" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58316" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/js/message.js
+++ src/js/message.js
@@ -6,41 +6,52 @@ module.exports = {
 
 // draw hangman stickfigure using canvas API
 
-// draw straight lines on canvas
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  ctx.moveTo(10,320);
-  ctx.lineTo(120,320);
-  ctx.stroke();
-  ctx.moveTo(65,60);
-  ctx.lineTo(65,320);
-  ctx.stroke();
-  ctx.moveTo(65,60);
-  ctx.lineTo(220,60);
-  ctx.stroke();
-  ctx.moveTo(220,60);
-  ctx.lineTo(220,110);
-  ctx.stroke();
+// create function that displays canvas hanger when page loads
+  window.onload = function displayHanger() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.moveTo(10,320);
+    ctx.lineTo(120,320);
+    ctx.stroke();
+    ctx.moveTo(65,60);
+    ctx.lineTo(65,320);
+    ctx.stroke();
+    ctx.moveTo(65,60);
+    ctx.lineTo(220,60);
+    ctx.stroke();
+    ctx.moveTo(220,60);
+    ctx.lineTo(220,110);
+    ctx.stroke();
+  }
 
-// draw a circle on canvas
-  var canvas = document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  ctx.beginPath();
-  ctx.arc(220, 140, 30, 0, 2 * Math.PI);
-  ctx.stroke();
 
-  ctx.moveTo(220,170);
-  ctx.lineTo(220,250);
-  ctx.stroke();
-  ctx.moveTo(220,250);
-  ctx.lineTo(180,300);
-  ctx.stroke();
-  ctx.moveTo(220,250);
-  ctx.lineTo(260,300);
-  ctx.stroke();
-  ctx.moveTo(220,210);
-  ctx.lineTo(180,190);
-  ctx.stroke();
-  ctx.moveTo(220,210);
-  ctx.lineTo(260,190);
-  ctx.stroke();
+// hanging hangman
+//display hangman onload
+//hide hangman figure
+//reveal a part of stick figure for every incorrect guess
+  // var canvas = document.getElementById("myCanvas");
+  // var ctx = canvas.getContext("2d");
+  // ctx.beginPath();
+  // ctx.arc(220, 140, 30, 0, 2 * Math.PI);
+  // ctx.stroke();
+
+  // ctx.moveTo(220,170);
+  // ctx.lineTo(220,250);
+  // ctx.stroke();
+  // ctx.moveTo(220,250);
+  // ctx.lineTo(180,300);
+  // ctx.stroke();
+  // ctx.moveTo(220,250);
+  // ctx.lineTo(260,300);
+  // ctx.stroke();
+  // ctx.moveTo(220,210);
+  // ctx.lineTo(180,195);
+  // ctx.stroke();
+  // ctx.moveTo(220,210);
+  // ctx.lineTo(260,195);
+  // ctx.stroke();
+
+  // create JS function to respond to player 1 button by adding text onClick (acceptWord)
+  //create function to display blank spaces (displayBlankSpaces)
+  //create function to hide word entry display?
+  //create function to process letter (processLetter)
