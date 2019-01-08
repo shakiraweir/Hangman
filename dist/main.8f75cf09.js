825279342f77f3c7bb78f3010b772992d7c3e4a8
Shakira Weir <shakira.weir87@gmail.com>
2019-01-08 15:47:39 -0500

    added text button for each player

--- dist/main.8f75cf09.js
+++ dist/main.8f75cf09.js
@@ -108,8 +108,35 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 module.exports = {
   message: function message(messageText) {
     console.log(messageText);
-  }
+  } // draw straight lines on canvas
+
 };
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.moveTo(10, 320);
+ctx.lineTo(120, 320);
+ctx.stroke();
+ctx.moveTo(65, 60);
+ctx.lineTo(65, 320);
+ctx.stroke();
+ctx.moveTo(65, 60);
+ctx.lineTo(220, 60);
+ctx.stroke();
+ctx.moveTo(220, 60);
+ctx.lineTo(220, 110);
+ctx.stroke(); // draw a circle on canvas
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.arc(220, 140, 30, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.moveTo(220, 170);
+ctx.lineTo(220, 250);
+ctx.stroke();
+ctx.moveTo(220, 250);
+ctx.lineTo(160, 310);
+ctx.stroke();
 },{}],"../js/main.js":[function(require,module,exports) {
 var messages = require("./message");
 
@@ -141,7 +168,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61115" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51213" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/js/message.js
+++ src/js/message.js
@@ -3,3 +3,33 @@ module.exports = {
     console.log(messageText)
   }
 }
+
+
+// draw straight lines on canvas
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.moveTo(10,320);
+  ctx.lineTo(120,320);
+  ctx.stroke();
+  ctx.moveTo(65,60);
+  ctx.lineTo(65,320);
+  ctx.stroke();
+  ctx.moveTo(65,60);
+  ctx.lineTo(220,60);
+  ctx.stroke();
+  ctx.moveTo(220,60);
+  ctx.lineTo(220,110);
+  ctx.stroke();
+
+// draw a circle on canvas
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(220, 140, 30, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.moveTo(220,170);
+  ctx.lineTo(220,250);
+  ctx.stroke();
+  ctx.moveTo(220,250);
+  ctx.lineTo(160,310);
+  ctx.stroke();
